882b0a41121077fbfb83c75828f003409533aebb
Shalin Abhay Shah <scs534@nyu.edu>
2016-06-03 00:47:30 -0400

    Add files via upload

---
+++ consts.py
@@ -0,0 +1,23 @@
+dict_acl = {
+    "eigrp":"88",
+    "eisp":"50",
+    "nos":"not supported",
+    "pcp":"not supported",
+    "precedence":"not supported"
+}
+
+
+dict_route_map = {
+    "pop":"not supported",
+    "push":"not supported",
+    "where":"not supported",
+    "length":"not supported",
+    "mac-list":"not supported",
+    "route-type":"metric-type",     # only supports type-1/type-2 
+    "external":"not supported",     #match route-type ?
+    "internal":"not supported",     #match route-type ?
+    "level-1":"not supported",      #match route-type ?
+    "level-2":"not supported",      #match route-type ?
+    "local":"not supported",        #match route-type ?
+    "nssa-external":"not supported" #match route-type ?
+}
